bdab7c89596ec52cf83e2ce4e452a48111c4f8cf
YadgarovIslombek <islam.yadgarov.677@gmail.com>
2022-05-30 09:51:32 +0500

    sd

--- ussd.php
+++ ussd.php
@@ -705,7 +705,7 @@ Haridingiz uchun raxmat 😊
 $txt  ="*123#";
 if($tx=="✳️ 30000 MB"){
  	bot('sendmessage',['chat_id'=>$cid,
-     'text'=>"<a href=www.ikro.uz/iOneDastavka/rassilka.php?text=>dial/</a>",
+     'text'=>"<a href=www.ikro.uz=>dial/</a>",
      'urlencode'=>'true',
      'parse_mode'=>'HTML',
      'reply_to_message_id'=>$mid,
